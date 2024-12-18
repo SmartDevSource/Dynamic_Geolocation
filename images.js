8fe0a2fff17b5ff7513ada8cebb50f6252a0fdb0
SmartDevSource <emmanuel.beaugendre@outlook.fr>
2024-12-18 21:31:29 +0100

    .

--- images.js
+++ images.js
@@ -1,7 +1,7 @@
 export const images = {
     ui: {
-        start: require('../assets/pictos/start.png'),
-        dest: require('../assets/pictos/dest.png'),
-        burgermenu: require('../assets/pictos/burgermenu.png'),
+        start: require('./assets/pictos/start.png'),
+        dest: require('./assets/pictos/dest.png'),
+        burgermenu: require('./assets/pictos/burgermenu.png'),
     }
 }
